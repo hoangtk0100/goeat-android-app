f7e489f5fa5c1c05363daa6459dfddda3e1a7729
sharingan0100 <sharingan.0100@gmail.com>
2018-06-08 00:49:23 +0700

    Initial commit

---
+++ app/src/main/java/tkh_15110211/vn/edu/hcmute/goeat/MainActivity.java
@@ -0,0 +1,32 @@
+package tkh_15110211.vn.edu.hcmute.goeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnAdd;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+        btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child("users").child("user1").setValue("hoang1");
+            }
+        });
+    }
+}
