0e02afdb83f30a7f671829b27cefbc50b31c5aa3
sharingan0100 <sharingan.0100@gmail.com>
2018-06-09 20:36:39 +0700

    Register & Login with Email_Password

--- app/src/main/java/tkh_15110211/vn/edu/hcmute/goeat/MainActivity.java
+++ app/src/main/java/tkh_15110211/vn/edu/hcmute/goeat/MainActivity.java
@@ -1,40 +1,15 @@
 package tkh_15110211.vn.edu.hcmute.goeat;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnAdd;
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference databaseReference;
-
-    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_login);
-/*
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference();
-
-        btnAdd = findViewById(R.id.btnAdd);
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                databaseReference.child("users").child("user1").setValue("hoang1");
-            }
-        });*/
+        setContentView(R.layout.activity_main);
 
     }
 }
