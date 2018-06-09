d0900395d261097bd7cea298bee1f84a928548d3
sharingan0100 <sharingan.0100@gmail.com>
2018-06-09 16:43:06 +0700

    connect Firebase

--- app/src/main/java/tkh_15110211/vn/edu/hcmute/goeat/MainActivity.java
+++ app/src/main/java/tkh_15110211/vn/edu/hcmute/goeat/MainActivity.java
@@ -1,10 +1,15 @@
 package tkh_15110211.vn.edu.hcmute.goeat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -13,20 +18,23 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
 
+    TextView txt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
-
+/*
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
 
-        btnAdd = findViewById(R.id.btnDangNhapFacebook);
+        btnAdd = findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 databaseReference.child("users").child("user1").setValue("hoang1");
             }
-        });
+        });*/
+
     }
 }
